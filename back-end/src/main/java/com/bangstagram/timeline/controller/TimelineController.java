2e1c7bcc5c77a54a5f8be15229892a522a97c5e4
Baeji77 <hahawjstk@gmail.com>
2020-04-29 00:34:30 +0900

    Update response object dto to responseEntity Because responseEntity can have http status easy.

--- back-end/src/main/java/com/bangstagram/timeline/controller/TimelineController.java
+++ back-end/src/main/java/com/bangstagram/timeline/controller/TimelineController.java
@@ -4,6 +4,8 @@ import com.bangstagram.timeline.dto.TimelineRequestDto;
 import com.bangstagram.timeline.dto.TimelineResponseDto;
 import com.bangstagram.timeline.service.TimelineService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -23,9 +25,9 @@ public class TimelineController {
 
     @PostMapping(TIMELINES)
     @ResponseBody
-    public TimelineResponseDto createTimeline (@RequestBody @Valid TimelineRequestDto timelineRequestDto) {
+    public ResponseEntity createTimeline (@RequestBody @Valid TimelineRequestDto timelineRequestDto) {
         TimelineResponseDto timelineResponseDto = timelineService.createNewTimeline(timelineRequestDto);
         log.info("{}", timelineResponseDto);
-        return timelineResponseDto;
+        return new ResponseEntity<>(timelineResponseDto, HttpStatus.CREATED);
     }
 }
