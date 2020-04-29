e8c735f15440515206534259ab2cb7493b6762a7
Baeji77 <hahawjstk@gmail.com>
2020-04-29 19:08:39 +0900

    Include custom exception "DoNotExistException" If target timelineId do not exist, occur DoNotExistException.

--- back-end/src/main/java/com/bangstagram/timeline/service/TimelineService.java
+++ back-end/src/main/java/com/bangstagram/timeline/service/TimelineService.java
@@ -1,6 +1,7 @@
 package com.bangstagram.timeline.service;
 
 
+import com.bangstagram.exception.DoNotExistException;
 import com.bangstagram.timeline.domain.model.Timeline;
 import com.bangstagram.timeline.domain.repository.TimelineRepository;
 import com.bangstagram.timeline.dto.TimelineRequestDto;
@@ -9,6 +10,11 @@ import com.bangstagram.timeline.dto.TimelineUpdateRequestDto;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+/**
+ author: Ji-Hoon Bae
+ Date: 2020.04.28
+ */
 
 @Service
 @Slf4j
@@ -28,7 +34,7 @@ public class TimelineService {
     @Transactional
     public TimelineResponseDto updateTimeline (Long timelineId, TimelineUpdateRequestDto timelineUpdateRequestDto) {
         log.info("{} {}", timelineId, timelineUpdateRequestDto);
-        Timeline foundTimeline = timelineRepository.findById(timelineId).orElseThrow(NullPointerException::new);
+        Timeline foundTimeline = timelineRepository.findById(timelineId).orElseThrow(() -> new DoNotExistException("There is not target id in database. timelineId: ", timelineId));
         foundTimeline.update(timelineUpdateRequestDto.getTitle(), timelineUpdateRequestDto.getBody());
         return new TimelineResponseDto(foundTimeline);
     }
