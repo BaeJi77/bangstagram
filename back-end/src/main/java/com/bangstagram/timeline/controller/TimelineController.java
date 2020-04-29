84ad758180f394e65c9a942243e5260222ba49de
Baeji77 <hahawjstk@gmail.com>
2020-04-29 12:56:28 +0900

    Implement update logic about timeline model Add /timelines/{timelineId} path. This need to update id and title, body data. UpdateTimeline method in service layer work find target id. If data is not, occur error. But data is, use update method in timeline model class.

--- back-end/src/main/java/com/bangstagram/timeline/controller/TimelineController.java
+++ back-end/src/main/java/com/bangstagram/timeline/controller/TimelineController.java
@@ -2,14 +2,13 @@ package com.bangstagram.timeline.controller;
 
 import com.bangstagram.timeline.dto.TimelineRequestDto;
 import com.bangstagram.timeline.dto.TimelineResponseDto;
+import com.bangstagram.timeline.dto.TimelineUpdateRequestDto;
 import com.bangstagram.timeline.service.TimelineService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -26,8 +25,16 @@ public class TimelineController {
     @PostMapping(TIMELINES)
     @ResponseBody
     public ResponseEntity createTimeline (@RequestBody @Valid TimelineRequestDto timelineRequestDto) {
-        TimelineResponseDto timelineResponseDto = timelineService.createNewTimeline(timelineRequestDto);
-        log.info("{}", timelineResponseDto);
-        return new ResponseEntity<>(timelineResponseDto, HttpStatus.CREATED);
+        TimelineResponseDto newTimeline = timelineService.createNewTimeline(timelineRequestDto);
+        log.info("{}", newTimeline);
+        return new ResponseEntity<>(newTimeline, HttpStatus.CREATED);
+    }
+
+    @PutMapping(TIMELINES + "/{timelineId}")
+    @ResponseBody
+    public ResponseEntity updateTimeline (@PathVariable("timelineId") Long timelineId, @RequestBody @Valid TimelineUpdateRequestDto timelineUpdateRequestDto) {
+        TimelineResponseDto updatedResult = timelineService.updateTimeline(timelineId, timelineUpdateRequestDto);
+        log.info("{}", updatedResult);
+        return new ResponseEntity<>(updatedResult, HttpStatus.CREATED);
     }
 }
--- back-end/src/main/java/com/bangstagram/timeline/service/TimelineService.java
+++ back-end/src/main/java/com/bangstagram/timeline/service/TimelineService.java
@@ -5,8 +5,10 @@ import com.bangstagram.timeline.domain.model.Timeline;
 import com.bangstagram.timeline.domain.repository.TimelineRepository;
 import com.bangstagram.timeline.dto.TimelineRequestDto;
 import com.bangstagram.timeline.dto.TimelineResponseDto;
+import com.bangstagram.timeline.dto.TimelineUpdateRequestDto;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Slf4j
@@ -21,5 +23,13 @@ public class TimelineService {
         log.info("{}", timelineRequestDto);
         Timeline newTimeline = timelineRepository.save(timelineRequestDto.convertTimelineRequestDtoToTimelineModel());
         return new TimelineResponseDto(newTimeline);
+    }
+
+    @Transactional
+    public TimelineResponseDto updateTimeline (Long timelineId, TimelineUpdateRequestDto timelineUpdateRequestDto) {
+        log.info("{} {}", timelineId, timelineUpdateRequestDto);
+        Timeline foundTimeline = timelineRepository.findById(timelineId).orElseThrow(NullPointerException::new);
+        foundTimeline.update(timelineUpdateRequestDto.getTitle(), timelineUpdateRequestDto.getBody());
+        return new TimelineResponseDto(foundTimeline);
     }
 }
