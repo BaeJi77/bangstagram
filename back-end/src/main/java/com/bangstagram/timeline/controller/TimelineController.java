5438eb25c42af7e25699e6e920098ac81862e301
Baeji77 <hahawjstk@gmail.com>
2020-04-30 14:43:30 +0900

    Change Controller to RestController So, remove ResponseBody anotation. And check convention.

--- back-end/src/main/java/com/bangstagram/timeline/controller/TimelineController.java
+++ back-end/src/main/java/com/bangstagram/timeline/controller/TimelineController.java
@@ -6,17 +6,17 @@ import com.bangstagram.timeline.service.TimelineService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 
-@Controller
+@RestController
 @Slf4j
 public class TimelineController {
     private static final String TIMELINES = "/timelines";
+
     private final TimelineService timelineService;
 
     public TimelineController(TimelineService timelineService) {
@@ -24,7 +24,6 @@ public class TimelineController {
     }
 
     @PostMapping(TIMELINES)
-    @ResponseBody
     public ResponseEntity createTimeline (@RequestBody @Valid TimelineRequestDto timelineRequestDto) {
         TimelineResponseDto timelineResponseDto = timelineService.createNewTimeline(timelineRequestDto);
         log.info("{}", timelineResponseDto);
