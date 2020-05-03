73fed2aa0b4c743ed2c8e6718224491f728a743e
eohyemin <eohyemin@gmail.com>
2020-05-03 23:04:05 +0900

    RoomControllerTest 수정

--- back-end/src/test/java/com/bangstagram/room/controller/RoomControllerTest.java
+++ back-end/src/test/java/com/bangstagram/room/controller/RoomControllerTest.java
@@ -2,7 +2,7 @@ package com.bangstagram.room.controller;
 
 import com.bangstagram.room.controller.dto.RoomResponseDto;
 import com.bangstagram.room.service.RoomService;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -14,11 +14,11 @@ import java.util.Collections;
 import static org.hamcrest.Matchers.*;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(RoomController.class)
 public class RoomControllerTest {
-
     @Autowired
     private MockMvc mockMvc;
 
@@ -26,20 +26,41 @@ public class RoomControllerTest {
     private RoomService roomService;
 
     @Test
-    public void room_getAll_test() throws Exception {
+    @DisplayName("방탈출 리스트 조회 테스트")
+    void findAllRooms() throws Exception {
         RoomResponseDto room = RoomResponseDto.builder()
                 .title("room_title")
                 .link("link")
                 .phone("phone")
                 .address("addr")
                 .description("desc").build();
-        given(roomService.getAll()).willReturn(Collections.singletonList(room));
+        given(roomService.findAll()).willReturn(Collections.singletonList(room));
 
         mockMvc.perform(get("/rooms"))
-                .andExpect(status().isOk())
+                .andExpect(status().isFound())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
                 .andExpect(jsonPath("$", iterableWithSize(1)))
-                .andExpect(jsonPath("$[0]['title']", is(equalTo("room_title"))));
-
+                .andExpect(jsonPath("$[0]['title']", is(equalTo("room_title"))))
+                .andDo(print());
     }
+
+    @Test
+    void findById() throws Exception {
+        RoomResponseDto room = RoomResponseDto.builder()
+                .id(1L)
+                .title("room_title")
+                .link("link")
+                .phone("phone")
+                .address("addr")
+                .description("desc")
+                .build();
+        given(roomService.findById(room.getId())).willReturn(room);
+
+        mockMvc.perform(get("/rooms/1"))
+                .andExpect(status().isFound())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.title", is(equalTo("room_title"))))
+                .andDo(print());
+    }
+
 }
