9adbf80433b574cb4b1f6c0c1978d2958ff83c0d
Baeji77 <hahawjstk@gmail.com>
2020-05-04 16:00:11 +0900

    Implement find timeline using by userId 1. Add timeline response logic in controller. 2. Return list of timelineResponseDto using stream and lambda. 3. Make findByUserId in TimelineRepository return list of timeline object.

--- back-end/src/main/java/com/bangstagram/timeline/controller/TimelineController.java
+++ back-end/src/main/java/com/bangstagram/timeline/controller/TimelineController.java
@@ -6,11 +6,10 @@ import com.bangstagram.timeline.service.TimelineService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @Slf4j
@@ -24,9 +23,15 @@ public class TimelineController {
     }
 
     @PostMapping(TIMELINES)
-    public ResponseEntity createTimeline (@RequestBody @Valid TimelineRequestDto timelineRequestDto) {
+    public ResponseEntity createTimeline(@RequestBody @Valid TimelineRequestDto timelineRequestDto) {
         TimelineResponseDto timelineResponseDto = timelineService.createNewTimeline(timelineRequestDto);
         log.info("{}", timelineResponseDto);
         return new ResponseEntity<>(timelineResponseDto, HttpStatus.CREATED);
+    }
+
+    @GetMapping(TIMELINES + "/{userId}")
+    public ResponseEntity getTimelineByUserId(@PathVariable Long userId) {
+        List<TimelineResponseDto> timelineResponseDtoList = timelineService.getTimelineByUserId(userId);
+        return new ResponseEntity<>(timelineResponseDtoList, HttpStatus.OK);
     }
 }
--- back-end/src/main/java/com/bangstagram/timeline/service/TimelineService.java
+++ back-end/src/main/java/com/bangstagram/timeline/service/TimelineService.java
@@ -7,6 +7,10 @@ import com.bangstagram.timeline.dto.TimelineRequestDto;
 import com.bangstagram.timeline.dto.TimelineResponseDto;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 
 @Service
@@ -18,7 +22,7 @@ public class TimelineService {
         this.timelineRepository = timelineRepository;
     }
 
-    public TimelineResponseDto createNewTimeline (TimelineRequestDto timelineRequestDto) {
+    public TimelineResponseDto createNewTimeline(TimelineRequestDto timelineRequestDto) {
         log.info("{}", timelineRequestDto);
         Timeline newTimeline = timelineRepository.save(timelineRequestDto.convertToTimeline());
         return TimelineResponseDto.builder()
@@ -29,5 +33,19 @@ public class TimelineService {
                 .roomId(newTimeline.getRoomId())
                 .createdAt(newTimeline.getCreatedAt())
                 .build();
+    }
+
+    @Transactional(readOnly = true)
+    public List<TimelineResponseDto> getTimelineByUserId(Long userId) {
+        return timelineRepository.findByUserId(userId).stream()
+                .map(timeline -> TimelineResponseDto.builder()
+                        .id(timeline.getId())
+                        .title(timeline.getTitle())
+                        .body(timeline.getBody())
+                        .createdAt(timeline.getCreatedAt())
+                        .userId(timeline.getUserId())
+                        .roomId(timeline.getRoomId())
+                        .build())
+                .collect(Collectors.toList());
     }
 }
